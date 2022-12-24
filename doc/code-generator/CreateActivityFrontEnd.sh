ebe3446de0032c97906b2328ec2455a0c6ea872a
ABID Aziz <83600576+Aziz19979@users.noreply.github.com>
2022-12-24 19:29:33 +0100

    Frontend code generator require src location only

--- doc/code-generator/CreateActivityFrontEnd.sh
+++ doc/code-generator/CreateActivityFrontEnd.sh
@@ -16,18 +16,13 @@
 
 # project dependent initialization
 FRONT_END_REACT_LOCATION="hceres-frontend"
-COMPONENTS="hceres-frontend/src/components/Activity"
-SERVICES="hceres-frontend/src/services"
 TemplateEntity="Education" ## Use CamelCase writing for template model
 
 # initialize subfolder locations from content root
 PROJECT_LOCATION=$(pwd)
+SRC_LOCATION="$FRONT_END_REACT_LOCATION/src"
 SCRIPT_FOLDER_LOCATION=$(dirname "$0")
 GENERATED_CODE_FOLDER="$PROJECT_LOCATION/GeneratedCode"
-COMPONENTS_LOCATION="$PROJECT_LOCATION/$COMPONENTS"
-SERVICES_LOCATION="$PROJECT_LOCATION/$SERVICES"
-
-echo "$1"
 
 if [ $# == 0 ]; then
   echo >&2 "No ModelEntity name was provided"
@@ -70,87 +65,75 @@ CamelCase_to_separate_by_dash() {
     <<<"$1"
 }
 
-template_package=$(CamelCase_to_separate_by_dash "$TemplateEntity")
-templateComponent="$COMPONENTS_LOCATION/$template_package"
-templateService="$SERVICES_LOCATION/$template_package"
 
 # check component folder and template pacakge if exist
-check_if_folder_exist "$COMPONENTS_LOCATION"
-check_if_folder_exist "$templateComponent"
-check_if_folder_exist "$SERVICES_LOCATION"
-check_if_folder_exist "$templateService"
+check_if_folder_exist "$SRC_LOCATION"
 
 echo "$ModelEntity is used"
 
 # initialize package name and targetLocations
+template_package=$(CamelCase_to_separate_by_dash "$TemplateEntity")
 target_package=$(CamelCase_to_separate_by_dash "$ModelEntity")
-targetComponent="$GENERATED_CODE_FOLDER/$COMPONENTS/"
-targetService="$GENERATED_CODE_FOLDER/$SERVICES/"
 
 # clear Generated code folder
 targetLocation="$GENERATED_CODE_FOLDER/$FRONT_END_REACT_LOCATION"
 rm -r "$targetLocation"
 echo "Code will be generated in: $GENERATED_CODE_FOLDER"
 
-# Create target locations
-mkdir -p "$targetComponent"
-mkdir -p "$targetService"
-
-echo "$targetComponent is created!"
-echo "$targetService is created!"
-
-# Copy templates files
-cp -r "$templateComponent" "$targetComponent"
-cp -r "$templateService" "$targetService"
-echo "Templates are copied!"
-
-# Rename pacakge as template pacakge
-mv "$targetComponent/$template_package" "$targetComponent/$target_package"
-mv "$targetService/$template_package" "$targetService/$target_package"
-echo "Package $template_package renamed to $target_package"
-
-# Traverse all files in Generated code recursively and perform following steps:
-# For each file:
-# 1. rename file TemplateEntity to ModelEntity
+# Traverse all files in $SRC_LOCATION recursively and perform following steps:
+# For each file pattern of *TemplateEntity*:
+# 0. copy file TemplateEntity to $GENERATED_CODE_FOLDER with same parent structure
+# 1. rename file template-entity/TemplateEntity to model-entity/ModelEntity
 # 2. replace all occurrences of template_package name to target_package
 # 3. replace all occurrences of TemplateEntity to ModelEntity
 # 4. replace all occurrences of templateEntity to modelEntity
+# 5. replace all plurals name having y at the end with ies
+
+# Create an array for storing the created files, if one does not already exist,
+# and print the contents of the array at the end
 if [ -z ${allCreatedFixedFiles+x} ]; then declare -A allCreatedFixedFiles; fi
 
 while IFS= read -r -d '' templateFile; do
   ((count++))
   echo "Found file no. $count"
+
+  # 0. copy file TemplateEntity to $GENERATED_CODE_FOLDER with same parent structure
+  cp --parent "$templateFile" "$GENERATED_CODE_FOLDER"
+  templateFile="$GENERATED_CODE_FOLDER/$templateFile"
   echo "$templateFile"
-  if grep -q "$TemplateEntity" "$templateFile"; then
-    # 1. rename file TemplateEntity to ModelEntity
-    # ${variable//search/replace}
-    modelFile=${templateFile//"$TemplateEntity"/"$ModelEntity"}
-    mv "$templateFile" "$modelFile"
-    echo "Renamed file to $modelFile"
 
-    # 2. replace all occurrences of template_package name to target_package
-    sed -i "s/\/$template_package/\/$target_package/g" "$modelFile"
-    echo "Replaced /$template_package with /$target_package"
+  # 1. rename file template-entity/TemplateEntity to model-entity/ModelEntity
+  # ${variable//search/replace}
+  modelFile=${templateFile//"$TemplateEntity"/"$ModelEntity"}
+  modelFile=${modelFile//"$template_package"/"$target_package"}
 
-    # 3. replace all occurrences of TemplateEntity to ModelEntity
-    sed -i "s/$TemplateEntity/$ModelEntity/g" "$modelFile"
-    echo "Replaced $TemplateEntity with $ModelEntity"
+  mkdir -p "$(dirname "$modelFile")"
+  mv "$templateFile" "$modelFile"
+  rm -r "$(dirname "$templateFile")"
+  echo "Renamed file to $modelFile"
 
-    # 4. replace all occurrences of templateEntity to ModelEntity
-    templateEntity="$(tr '[:upper:]' '[:lower:]' <<<${TemplateEntity:0:1})${TemplateEntity:1}"
-    modelEntity="$(tr '[:upper:]' '[:lower:]' <<<${ModelEntity:0:1})${ModelEntity:1}"
-    sed -i "s/$templateEntity/$modelEntity/g" "$modelFile"
-    echo "Replaced $templateEntity with $modelEntity "
-    allCreatedFixedFiles+=(["$count"]="$modelFile")
+  # 2. replace all occurrences of template_package name to target_package
+  sed -i "s/\/$template_package/\/$target_package/g" "$modelFile"
+  echo "Replaced /$template_package with /$target_package"
 
-    # 5. replace all plurals name having y at the end with ies
-    lastCharacterIndex=${#ModelEntity}-1
-    if [ "${ModelEntity:$lastCharacterIndex}" = "y" ]; then
-      sed -i "s/${ModelEntity:1}s/${ModelEntity:1:$lastCharacterIndex-1}ies/g" "$modelFile"
-      echo "Replaced ${ModelEntity:1}s with ${ModelEntity:1:$lastCharacterIndex-1}ies "
-    fi
+  # 3. replace all occurrences of TemplateEntity to ModelEntity
+  sed -i "s/$TemplateEntity/$ModelEntity/g" "$modelFile"
+  echo "Replaced $TemplateEntity with $ModelEntity"
+
+  # 4. replace all occurrences of templateEntity to ModelEntity
+  templateEntity="$(tr '[:upper:]' '[:lower:]' <<<${TemplateEntity:0:1})${TemplateEntity:1}"
+  modelEntity="$(tr '[:upper:]' '[:lower:]' <<<${ModelEntity:0:1})${ModelEntity:1}"
+  sed -i "s/$templateEntity/$modelEntity/g" "$modelFile"
+  echo "Replaced $templateEntity with $modelEntity "
+  allCreatedFixedFiles+=(["$count"]="$modelFile")
+
+  # 5. replace all plurals name having y at the end with ies
+  lastCharacterIndex=${#ModelEntity}-1
+  if [ "${ModelEntity:$lastCharacterIndex}" = "y" ]; then
+    sed -i "s/${ModelEntity:1}s/${ModelEntity:1:$lastCharacterIndex-1}ies/g" "$modelFile"
+    echo "Replaced ${ModelEntity:1}s with ${ModelEntity:1:$lastCharacterIndex-1}ies "
   fi
-done < <(find "$GENERATED_CODE_FOLDER" -mtime -7 -name '*.js' -print0)
+done < <(find "$SRC_LOCATION" -name "*$TemplateEntity*js" -print0)
 
 if [ -n "$csvDataFile" ]; then
   source "$SCRIPT_FOLDER_LOCATION/CreateFormBootstrap.sh" "$csvDataFile" "$ModelEntity"
