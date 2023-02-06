716223802225f51c7ff18780b982339074f9fe06
MeryamAssermouh <95424934+MeryamAssermouh@users.noreply.github.com>
2023-02-06 07:42:11 +0100

    Travail fait aujd par Meryam
    J'ai rajouté:
    - onglet statistiques
    - pages statistiques avec le menu clickable
    - une directory pour chaque menu

--- hceres-frontend/src/components/Statistiques/Statistiques.js
+++ hceres-frontend/src/components/Statistiques/Statistiques.js
@@ -1,4 +1,23 @@
-export default function Statistiques () {
+import React from 'react';
 
-    return "this is Statistiques page"
+export default function Statistiques () {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{ display: 'flex', flexDirection: 'column' , alignItems: 'center' }}>
+                <h1 style={{ fontSize: 24, marginBottom: 20 }}>Statistiques</h1>
+                <div style={{ width: 200, height: 50, marginBottom: 10, display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightgray' }}>
+                    <a href="/publications" style={{ textDecoration: 'none', color: 'black' }}>Publications</a>
+                </div>
+                <div style={{ width: 200, height: 50, marginBottom: 10, display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightgray' }}>
+                    <a href="/brevets" style={{ textDecoration: 'none', color: 'black' }}>Brevets</a>
+                </div>
+                <div style={{ width: 200, height: 50, marginBottom: 10, display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightgray' }}>
+                    <a href="/financements" style={{ textDecoration: 'none', color: 'black' }}>Financements</a>
+                </div>
+                <div style={{ width: 200, height: 50, marginBottom: 10, display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightgray' }}>
+                    <a href="/doctorats" style={{ textDecoration: 'none', color: 'black' }}>Doctorats</a>
+                </div>
+            </div>
+        </div>
+    );
 }
