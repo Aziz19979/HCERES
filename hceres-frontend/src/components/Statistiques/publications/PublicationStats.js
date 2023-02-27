d7675d01327ceeec963d5e90288415ab2e7ecb0c
MeryamAssermouh <95424934+MeryamAssermouh@users.noreply.github.com>
2023-02-27 13:46:51 +0100

    tentative d'ajout d'histogramme

--- hceres-frontend/src/components/Statistiques/publications/PublicationStats.js
+++ hceres-frontend/src/components/Statistiques/publications/PublicationStats.js
@@ -3,23 +3,79 @@ import {fetchListIncomingMobilities} from "../../../services/Activity/incoming-m
 import {fetchResearcherActivities} from "../../../services/Researcher/ResearcherActions";
 import ActivityTypes from "../../../const/ActivityTypes";
 import {fetchListPublications} from "../../../services/Activity/publication/PublicationActions";
+import { Chart } from 'chart.js/auto';
+import moment from 'moment';
 
-export default function PublicationStats () {
-    // Cached state (List Template)
-    const [publicationList, setPublicationList] = React.useState(null);
+export default function PublicationStats() {
+    const [chart, setChart] = React.useState(null);
+    const [publicationList, setPublicationList] = React.useState([]);
 
     React.useEffect(() => {
-        fetchListPublications().then(list => setPublicationList(list))
+        fetchListPublications().then((list) => setPublicationList(list));
     }, []);
-
 
     console.log(publicationList)
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <div style={{ display: 'flex', flexDirection: 'column' , alignItems: 'center' }}>
-                <h1 style={{ fontSize: 24, marginBottom: 20 }}>Des statistiques sur les publications</h1>
+    React.useEffect(() => {
+        if (publicationList.length > 0) {
+            const ctx = document.getElementById('myChart');
+            const years = publicationList.map((item) =>
+                moment(item.publicationDate).year()
+            );
 
-            </div>
-        </div>);
+            const count = {};
+            years.forEach((year) => {
+                count[year] = (count[year] || 0) + 1;
+            });
+
+            const data = {
+                labels: Object.keys(count),
+                datasets: [
+                    {
+                        label: 'Publications per year',
+                        data: Object.values(count),
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1,
+                    },
+                ],
+            };
+
+            if (chart !== null) {
+                chart.destroy();
+            }
+
+            const myChart = new Chart(ctx, {
+                type: 'bar',
+                data: data,
+                options: {
+                    responsive: true,
+                    scales: {
+                        yAxes: [
+                            {
+                                ticks: {
+                                    beginAtZero: true,
+                                },
+                            },
+                        ],
+                    },
+                },
+            });
+
+            setChart(myChart);
+        }
+
+        return () => {
+            if (chart !== null) {
+                chart.destroy();
+            }
+        };
+    }, [publicationList, chart]);
+
+    return (
+        <div>
+            <h2>Publications per year</h2>
+            <canvas id="myChart" width="400" height="400"></canvas>
+        </div>
+    );
 }
